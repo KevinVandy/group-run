4424963a20d102b2f181cd692ece2c0349f1fae4
Kevin Vandy <kevinvandy656@gmail.com>
2020-08-22 14:50:07 -0500

    Setup initial pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
-// import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { ThemeProvider, StylesProvider } from "@material-ui/core/styles";
+import { Chats } from "./pages/Chats";
+import { Groups } from "./pages/Groups";
+import { Log } from "./pages/Log";
+import { Profile } from "./pages/Profile";
+import { Login } from "./pages/Login";
 
 export const App = () => {
-  return <div className="App">Hello World</div>;
+  return (
+    <StylesProvider injectFirst>
+      <ThemeProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/chats" component={Chats} />
+            <Route exact path="/groups" component={Groups} />
+            <Route exact path="/log" component={Log} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/" component={Login} />
+          </Switch>
+        </BrowserRouter>
+      </ThemeProvider>
+    </StylesProvider>
+  );
 };
 
 export default App;
---
+++ src/pages/Groups.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Groups = () => {
+  return (
+    <div>
+      Groups Page
+    </div>
+  )
+}
